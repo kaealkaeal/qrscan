839bf60a9c5d78d8435deba873bb8cab41f80d78
Amonrat Loylertlar <kaealkaeal159@hotmail.com>
2018-10-25 06:14:00 +0700

    first commit

---
+++ docs/liff.js
@@ -0,0 +1,7 @@
+window.onload = function (e) {
+    // init で初期化。基本情報を取得。
+    // https://developers.line.me/ja/reference/liff/#initialize-liff-app
+    liff.init(function (data) {
+        getProfile();
+        initializeApp(data);
+    });
